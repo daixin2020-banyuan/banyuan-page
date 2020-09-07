25cb17677566391192ca2e73966f1319cef6f766
edz <edz@DaiXindeMacBook-Air.local>
2020-09-07 08:59:58 +0800

    半圆页面

--- static/index.js
+++ static/index.js
@@ -2,9 +2,26 @@ let titleBtn = document.getElementsByClassName('title-button')[0];
 let head = document.getElementsByClassName('header')[0];
 let menu = document.getElementsByClassName('right-menu')[0];
 let details = document.getElementsByClassName('menu-details');
-console.log(details)
+
+function reName(){
+    for(let i = 0 ; i <details.length;i++){
+        if(details[i].getAttribute('class') === 'menu-details on'){
+            details[i].setAttribute("class",'menu-details' )
+        }
+    }
+}
+// $('.menu-details').css('color','rgb(238, 186, 54)')
+// console.log($('.menu-details'))
 
 controlHeader();
+
+window.onload = function(){
+    
+        setTimeout(function(){ 
+                document.getElementsByTagName('body')[0].setAttribute("class",'' );
+        }, 10);
+   
+}
 
 $(window).scroll(function(){
     controlHeader();
@@ -18,6 +35,8 @@ $('.menu-details').click(function(event){
 
     if($(`.${href}`).length>0){
         document.documentElement.scrollTop= $(`.${href}`).offset().top
+        reName();
+        $(this).addClass("on")
     }
 })
 
@@ -46,101 +65,5 @@ function controlHeader(){
     
 }
 
-window.onscroll = function() {
-    let scrollTop = document.documentElement.scrollTop
-
-        if (scrollTop>1 && scrollTop <650){
-            document.getElementById('home').setAttribute("class",'red')
-            document.getElementById('about').setAttribute("class",'menu-details')
-            document.getElementById('service').setAttribute("class",'menu-details')
-            document.getElementById('blog').setAttribute("class",'menu-details')
-            document.getElementById('contact').setAttribute("class",'menu-details')
-        }
-        else if (scrollTop > 650 && scrollTop <1783){
-            document.getElementById('about').setAttribute("class",'red')
-            document.getElementById('home').setAttribute("class",'menu-details')
-            document.getElementById('service').setAttribute("class",'menu-details')
-            document.getElementById('blog').setAttribute("class",'menu-details')
-            document.getElementById('contact').setAttribute("class",'menu-details')
-        }else if (scrollTop > 1783 && scrollTop <2644){
-            document.getElementById('service').setAttribute("class",'red')
-            document.getElementById('home').setAttribute("class",'menu-details')
-            document.getElementById('about').setAttribute("class",'menu-details')
-            document.getElementById('blog').setAttribute("class",'menu-details')
-            document.getElementById('contact').setAttribute("class",'menu-details')
-        }else if(scrollTop > 2644 && scrollTop <3818){
-            document.getElementById('blog').setAttribute("class",'red')
-            document.getElementById('home').setAttribute("class",'menu-details')
-            document.getElementById('about').setAttribute("class",'menu-details')
-            document.getElementById('service').setAttribute("class",'menu-details')
-            document.getElementById('contact').setAttribute("class",'menu-details')
-        }else if(scrollTop >3818){
-            document.getElementById('contact').setAttribute("class",'red')
-            document.getElementById('home').setAttribute("class",'menu-details')
-            document.getElementById('about').setAttribute("class",'menu-details')
-            document.getElementById('service').setAttribute("class",'menu-details')
-            document.getElementById('blog').setAttribute("class",'menu-details')
-        }
-
-}
-    
 
 
-
-// window.onscroll = function(){
-//     var scrollTop = document.documentElement.scrollTop
-//     console.log(scrollTop)
-
-//     controlHeader();
-
-
-//     if( scrollTop <650 ){
-//         for(i = 0 ; i < 5;i++){
-//             if ( i = 0 ){
-//                 details[i].setAttribute("class",'red' )
-//             }else{
-//                 details[i].setAttribute("class",'menu-details' )
-//             }
-//         }
-//     }
-
-    // else if(scrollTop > 650 && scrollTop <1783){
-    //     for(i = 0 ; i < 5;i++){
-    //         if ( i = 1 ){
-    //             details[i].setAttribute("class",'red' )
-    //         }else{
-    //             details[i].setAttribute("class",'menu-details' )
-    //         }
-    //     }
-
-    // }
-    // else if(scrollTop > 1783 && scrollTop <2644){
-    //     for(i = 0 ; i < 5;i++){
-    //         if ( i = 2 ){
-    //             details[i].setAttribute("class",'red' )
-    //         }else{
-    //             details[i].setAttribute("class",'menu-details' )
-    //         }
-    //     }
-        
-    // }
-    // else if(scrollTop > 2644 && scrollTop <3818){
-    //     for(i = 0 ; i < 5;i++){
-    //         if ( i = 3 ){
-    //             details[i].setAttribute("class",'red' )
-    //         }else{
-    //             details[i].setAttribute("class",'menu-details' )
-    //         }
-    //     }
-
-    // }else if(scrollTop >3818){
-    //     for(i = 0 ; i < 5;i++){
-    //         if ( i = 4 ){
-    //             details[i].setAttribute("class",'red' )
-    //         }else{
-    //             details[i].setAttribute("class",'menu-details' )
-    //         }
-    //     }
-    // }
-
-// }
